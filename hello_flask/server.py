5361d38131406e405f5f79679ad21537e1d3a103
Tillman Pugh <Till.pugh@gmail.com>
2023-04-07 12:28:16 -0700

    first commit

--- hello_flask/server.py
+++ hello_flask/server.py
@@ -1,8 +1,22 @@
-# from flask import Flask  # Import Flask to allow us to create our app
-# app = Flask(__name__)    # Create a new instance of the Flask class called "app"
-# @app.route('/')          # The "@" decorator associates this route with the function immediately following
-# def hello_world():
-#     return 'Hello World!'  # Return the string 'Hello World!' as a response
-# if __name__=="__main__":   # Ensure this file is being run directly and not from a different module    
-#     app.run(debug=True)    # Run the app in debug mode.
+from flask import Flask, render_template
+app = Flask(__name__)    
+@app.route('/')          
+def hello_world():
+    return 'Hello World!'  
+
+@app.route('/lists')
+def render_lists():
+
+    student_info = [
+    {'name' : 'Michael', 'age' : 35},
+    {'name' : 'John', 'age' : 30 },
+    {'name' : 'Mark', 'age' : 25},
+    {'name' : 'KB', 'age' : 27}
+    ]
+    return render_template("index.html", random_numbers = [3,1,5], students = student_info)
+
+
+
+if __name__=="__main__":    
+    app.run(debug=True)    
 
